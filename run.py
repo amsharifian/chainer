f562444eb390152dbbb3fe067d0e37bc8d298e46
Amirali <am.sharifian@gmail.com>
2016-09-08 11:49:40 -0700

    Clean up run file

--- run.py
+++ run.py
@@ -1,22 +1,16 @@
 import pdb
 import sys
-import os
-import pygraphviz as pgv
 import networkx as nx
-from networkx.drawing.nx_pydot import write_dot
-import matplotlib
-matplotlib.use("Agg")
-import matplotlib.pyplot as plt
 import json
-from collections import OrderedDict
-import numpy as np
 from graph.dilworth import chains as ch
-from graph.limitio import limitio as lim
 from graph.limitio import limitcheck as check
 
+
 def main(dotfile):
+
+    # Reading
     # Reading dot file
-    ng = ch.CreateGraph(dotfile, [('style', '')]);
+    ng = ch.CreateGraph(dotfile, [('style', '')])
 
     # Check whether the graph has more than one BB node or not
     if ch.CheckStartNode(ng) == 1:
@@ -34,11 +28,12 @@ def main(dotfile):
     # Breaking head node
     ch.BreakHeadNode(ng)
 
-    check.checkEdgeOut(ng,2)
+    check.checkEdgeOut(ng, 2)
 
-    check.limitChainOut(ng,2)
+    check.limitChainOut(ng, 2)
 
-    check.limitChainIn(ng,2)
+    check.limitChainIn(ng, 2)
+
     # cg = nx.DiGraph()
     # lim.AddChainNodes(cg, ng)
 
@@ -56,13 +51,13 @@ def main(dotfile):
     nx.drawing.nx_pydot.write_dot(ng, 'sample.dot')
 
 
-
 nargs = len(sys.argv)
 if (nargs != 3):
         print 'usage: python chains.py <dot file> <output file>'
         sys.exit()
 
-dotfile = sys.argv[1] 
-fig = sys.argv[2]
-main(dotfile) 
 
+dotfile = sys.argv[1]
+fig = sys.argv[2]
+if __name__ == "__main__":
+    main(dotfile)
